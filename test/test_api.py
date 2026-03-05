6522fd11410f09444897789bc95caa7d71dab3ab
Zhang Kai <2496873728@qq.com>
2026-03-05 18:54:52 +0800

    feat

--- test/test_api.py
+++ test/test_api.py
@@ -1,5 +1,10 @@
 import os
 import dashscope
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path=r"F:/zhangkai/code/chatchat/.env")
+api_key=os.getenv("DASHSCOPE_API_KEY")
 
 messages = [
     {'role': 'system', 'content': 'You are a helpful assistant.'},
@@ -7,7 +12,7 @@ messages = [
 ]
 response = dashscope.Generation.call(
     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
-    api_key="sk-48581151e52849faa01799d4f33b2742",
+    api_key=api_key,
     model="qwen-plus", # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
     messages=messages,
     result_format='message'
