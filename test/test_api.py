857be652bdc9c205be0add3e6dbc772ec531c01f
Zhang Kai <2496873728@qq.com>
2026-03-05 18:32:43 +0800

    add api test

---
+++ test/test_api.py
@@ -0,0 +1,15 @@
+import os
+import dashscope
+
+messages = [
+    {'role': 'system', 'content': 'You are a helpful assistant.'},
+    {'role': 'user', 'content': '你是谁？'}
+]
+response = dashscope.Generation.call(
+    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
+    api_key="sk-48581151e52849faa01799d4f33b2742",
+    model="qwen-plus", # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    messages=messages,
+    result_format='message'
+    )
+print(response)
